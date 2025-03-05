bab2c9cdcfbfb27a97f28b4723cf6bdd63d43179
hssw24 <hssw@gmx.de>
2025-03-05 14:11:01 +0100

    Beschreibung der Änderungen

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const App = () => {
   const links = [
-    { label: '2. Klasse' },
-    { label: 'Zweite Seite', name: 'Silben Auswahl', url: 'https://silbenauswahl.vercel.app/' },
-    { label: 'Dritte Seite', name: 'Zehner Plus', url: 'https://zehnerplus.vercel.app/' },
     { label: '3. Klasse' },
+    { label: '10er-Übergang minus - Vorbereitung für "Schriftliche Subtraktion"', name: '10er-Übergang minus', url: 'https://10erm.vercel.app/' },
+    { label: 'Dritte Seite', name: 'Zehner Plus', url: 'https://zehnerplus.vercel.app/' },
+    { label: '2. Klasse' },
     { label: 'Vierte Seite', name: '10er-Übergang mit +', url: 'https://obst-one.vercel.app/' },
     { label: 'Fünfte Seite', name: '10er-Übergang mit -', url: 'https://verliebte-zahlen.vercel.app/' },
     { label: 'Sonstiges'},
